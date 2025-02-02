037132fb2271097acbb959e375a666bb98282584
Tulip Blossom <tulilirockz@outlook.com>
2025-02-02 09:40:09 -0300

    fix: dont install nvidia drivers on non-x86

--- build_scripts/overrides/hwe/10-hyperscale-kernel.sh
+++ build_scripts/overrides/hwe/10-hyperscale-kernel.sh
@@ -4,6 +4,8 @@ set -euox pipefail
 
 # The hyperscale SIG's kernel straight from their official builds
 dnf -y install centos-release-hyperscale-kernel
+dnf config-manager --set-disabled "centos-hyperscale"
+dnf config-manager --set-disabled "centos-hyperscale-kernel"
 dnf --enablerepo="centos-hyperscale" --enablerepo="centos-hyperscale-kernel" -y update kernel
 
 # Only necessary when not building with Nvidia
