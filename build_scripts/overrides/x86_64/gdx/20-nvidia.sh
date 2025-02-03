e675fdb13539d00c05434092302a25f6be9bb784
Tulip Blossom <tulilirockz@outlook.com>
2025-02-03 00:24:06 -0300

    feat: add cuda and fix nvidia build

--- build_scripts/overrides/x86_64/gdx/20-nvidia.sh
+++ build_scripts/overrides/x86_64/gdx/20-nvidia.sh
@@ -5,8 +5,9 @@ set -euox pipefail
 # FIXME: add renovate rules for this (somehow?)
 NVIDIA_DISTRO="rhel9"
 
-# kernel-devel, kernel-devel-matched and kernel-headers are necessary for nvidia drivers
-dnf -y install kernel-devel kernel-devel-matched kernel-headers
+# These are necessary for building the nvidia drivers
+# DKMS is provided by EPEL
+dnf -y install kernel-devel kernel-devel-matched kernel-headers dkms
 
 dnf config-manager --add-repo "https://developer.download.nvidia.com/compute/cuda/repos/${NVIDIA_DISTRO}/$(arch)/cuda-${NVIDIA_DISTRO}.repo"
 dnf clean expire-cache
@@ -15,12 +16,16 @@ NVIDIA_DRIVER_DIRECTORY=$(mktemp -d)
 # EGL-gbm and EGL-wayland fail to install because of conflicts with each other
 dnf download egl-gbm egl-wayland --destdir=$NVIDIA_DRIVER_DIRECTORY
 rpm -ivh $NVIDIA_DRIVER_DIRECTORY/*.rpm --nodeps --force
+
 dnf -y install --nogpgcheck \
   -x egl-wayland \
   -x egl-gbm \
-  nvidia-driver kmod-nvidia-open-dkms
-echo "blacklist nouveau" | tee /etc/modprobe.d/nouveau-blacklist.conf
-echo "options nouveau modeset=0" | tee -a /etc/modprobe.d/nouveau-blacklist.conf
+  nvidia-driver{,-cuda} kmod-nvidia-open-dkms
+
+cat >/etc/modprobe.d/nouveau-blacklist.conf <<EOF
+blacklist nouveau
+options nouveau modeset=0
+EOF
 
 # Make sure initramfs is rebuilt after nvidia drivers or kernel replacement
 KERNEL_SUFFIX=""
