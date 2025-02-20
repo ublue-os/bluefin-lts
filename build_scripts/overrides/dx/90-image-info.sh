3f5427c39a3983c9f81759468a9ceef2c17ac8af
Tulip Blossom <tulilirockz@outlook.com>
2025-02-19 21:10:51 -0300

    fix: use general script for image-info replacement

--- build_scripts/overrides/dx/90-image-info.sh
+++ build_scripts/overrides/dx/90-image-info.sh
@@ -3,13 +3,4 @@
 set -xeuo pipefail
 
 FLAVOR="dx"
-
-IMAGE_INFO="/usr/share/ublue-os/image-info.json"
-IMAGE_NAME="$(jq -c -r '."image-name"' "${IMAGE_INFO}")"
-IMAGE_REF="$(jq -c -r '."image-ref"' "${IMAGE_INFO}")"
-
-jq -f /dev/stdin "${IMAGE_INFO}" <<EOF
-."image-name" = "${IMAGE_NAME}-${FLAVOR}" | \
-."image-flavor" = "${FLAVOR}" | \
-."image-ref" = "${IMAGE_REF}-${FLAVOR}"
-EOF
+"${SCRIPTS_PATH}/image-info-set"
--- build_scripts/overrides/x86_64/gdx/90-image-info.sh
+++ build_scripts/overrides/x86_64/gdx/90-image-info.sh
@@ -3,14 +3,6 @@
 set -xeuo pipefail
 
 FLAVOR="gdx"
-
-IMAGE_INFO="/usr/share/ublue-os/image-info.json"
-IMAGE_NAME="$(jq -c -r '."image-name"' "${IMAGE_INFO}")"
-IMAGE_REF="ostree-image-signed:docker://ghcr.io/ublue-os/bluefin-gdx"
-
-
-jq -f /dev/stdin "${IMAGE_INFO}" <<EOF
-."image-name" = "bluefin-${FLAVOR}" | \
-."image-flavor" = "${FLAVOR}" | \
-."image-ref" = "${IMAGE_REF}"
-EOF
+IMAGE_NAME="bluefin-${FLAVOR}"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/ublue-os/bluefin-${FLAVOR}"
+"${SCRIPTS_PATH}/image-info-set"
