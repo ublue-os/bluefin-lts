2316294a433cae7c5514326f23bc7b540a24c55a
Robert Sturla <robertsturla@outlook.com>
2025-02-21 21:45:47 +0000

    feat: setup basic ARM GDX image

---
+++ build_scripts/overrides/aarch64/gdx/20-nvidia.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+# FIXME: add renovate rules for this (somehow?)
+NVIDIA_DISTRO="rhel9"
+
+dnf config-manager --add-repo "https://developer.download.nvidia.com/compute/cuda/repos/${NVIDIA_DISTRO}/$(arch)/cuda-${NVIDIA_DISTRO}.repo"
+dnf clean expire-cache
+
+dnf -y install \
+  nvidia-container-toolkit
+
+systemctl enable ublue-nvctk-cdi.service
