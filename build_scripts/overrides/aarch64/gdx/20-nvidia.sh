6087a80bd008abc1cd6dc461e92776de5cb7e17e
Robert Sturla <robertsturla@outlook.com>
2025-02-21 23:29:04 +0000

    fix: skip gpg check on nvidia container install

--- build_scripts/overrides/aarch64/gdx/20-nvidia.sh
+++ build_scripts/overrides/aarch64/gdx/20-nvidia.sh
@@ -8,7 +8,7 @@ NVIDIA_DISTRO="rhel9"
 dnf config-manager --add-repo "https://developer.download.nvidia.com/compute/cuda/repos/${NVIDIA_DISTRO}/$(arch)/cuda-${NVIDIA_DISTRO}.repo"
 dnf clean expire-cache
 
-dnf -y install \
+dnf -y install --nogpgcheck \
   nvidia-container-toolkit \
   libnvidia-container1
 
