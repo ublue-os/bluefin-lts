6ed7530f4b455f42eab0f8f71f1459ed2bf2d32b
Tulip Blossom <tulilirockz@outlook.com>
2025-02-26 19:26:55 -0300

    feat: make ublue user services work with helper script

---
+++ system_files/usr/share/ublue-os/privileged-setup.hooks.d/99-flatpaks.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+source /usr/lib/ublue/setup-services/libsetup.sh
+
+version-script flatpaks-lts privileged 1 || exit 0
+
+set -x
+
+# Set up Firefox default configuration
+ARCH=$(arch)
+if [ "$ARCH" != "aarch64"] ; then
+	mkdir -p "/var/lib/flatpak/extension/org.mozilla.firefox.systemconfig/${ARCH}/stable/defaults/pref"
+	rm -f "/var/lib/flatpak/extension/org.mozilla.firefox.systemconfig/${ARCH}/stable/defaults/pref/*bluefin*.js"
+	/usr/bin/cp -rf /usr/share/ublue-os/firefox-config/* "/var/lib/flatpak/extension/org.mozilla.firefox.systemconfig/${ARCH}/stable/defaults/pref/"
+fi
+
+# Use until yafti rework is done
+flatpak --system update -y
+
+notify-send "Welcome to Bluefin LTS" "Your computer is ready!" --app-name="Flatpak Manager Service" -u NORMAL
