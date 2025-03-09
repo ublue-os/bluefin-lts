0e2228bfbb74cede5cfa8d0f200310002bf0b74b
Tulip Blossom <tulilirockz@proton.me>
2025-03-09 00:50:21 -0300

    fix: ensure we are versioning the script too

--- system_files_overrides/gdx/usr/share/ublue-os/user-setup.hooks.d/30-gdx-vscode.sh
+++ system_files_overrides/gdx/usr/share/ublue-os/user-setup.hooks.d/30-gdx-vscode.sh
@@ -2,6 +2,10 @@
 
 set -xeuo pipefail
 
+source /usr/lib/ublue/setup-services/libsetup.sh
+
+version-script gdx-vscode-lts user 1 || exit 0
+
 code --install-extension NVIDIA.nsight-vscode-edition
 # cpptools is required by nsight-vscode
 code --install-extension ms-vscode.cpptools
