df997c2175caf2134ff8cca2fd4d7f23b05c4588
Tulip Blossom <tulilirockz@proton.me>
2025-03-09 00:52:05 -0300

    fix: reorder script

--- system_files_overrides/gdx/usr/share/ublue-os/user-setup.hooks.d/30-gdx-vscode.sh
+++ system_files_overrides/gdx/usr/share/ublue-os/user-setup.hooks.d/30-gdx-vscode.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
-
 source /usr/lib/ublue/setup-services/libsetup.sh
 
 version-script gdx-vscode-lts user 1 || exit 0
 
-code --install-extension NVIDIA.nsight-vscode-edition
+set -xeuo pipefail
+
 # cpptools is required by nsight-vscode
 code --install-extension ms-vscode.cpptools
+code --install-extension NVIDIA.nsight-vscode-edition
