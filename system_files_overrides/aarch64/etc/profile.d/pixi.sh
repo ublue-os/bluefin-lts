9df25bb563ca16fbeccfe7fa70cb3215a0b252e8
James Reilly <jreilly1821@gmail.com>
2025-03-17 01:45:41 +0530

    Update system_files_overrides/aarch64/etc/profile.d/pixi.sh
    Co-authored-by: Tulip Blossom <tulilirockz@outlook.com>

--- system_files_overrides/aarch64/etc/profile.d/pixi.sh
+++ system_files_overrides/aarch64/etc/profile.d/pixi.sh
@@ -6,10 +6,5 @@ PIXI_BIN_DIR="$HOME/.pixi/bin"
 if [[ -d "$PIXI_BIN_DIR" ]]; then
   if [[ ":$PATH:" != *":$PIXI_BIN_DIR:"* ]]; then
     export PATH="$PIXI_BIN_DIR:$PATH"
-    echo "Added pixi bin directory to PATH: $PIXI_BIN_DIR"
-  else
-    echo "pixi bin directory already in PATH."
   fi
-else
-  echo "pixi bin directory not found: $PIXI_BIN_DIR"
 fi
