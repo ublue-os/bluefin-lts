8e8843178306d7272f07e4a6be401eb332c6dcfd
James Reilly <jreilly1821@gmail.com>
2025-03-17 22:26:59 +0530

    fix(gdx-demo): correct syntax for including GDX demo in Just menu

--- build_scripts/overrides/aarch64/gdx/gdx-demo-just.sh
+++ build_scripts/overrides/aarch64/gdx/gdx-demo-just.sh
@@ -3,4 +3,4 @@
 set -xeuo pipefail
 
 #Add the GDX demo to the Just menu
-cat "include /usr/share/ublue-os/just/66-ampere.just" >> /usr/share/ublue-os/just/60-custom.just
+cat "include? /usr/share/ublue-os/just/66-ampere.just" >> /usr/share/ublue-os/just/60-custom.just
