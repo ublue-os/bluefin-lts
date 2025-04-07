95bad7ea9cd410cc232d32a6223c47b1098b5014
Robert Sturla <robertsturla@outlook.com>
2025-04-07 19:27:31 +0100

    chore: switch to anaconda webui

--- iso_files/configure_iso.sh
+++ iso_files/configure_iso.sh
@@ -10,6 +10,11 @@ dnf install -y \
   anaconda-install-env-deps \
   anaconda-live
 
+echo "ostreecontainer --url=/usr/lib/ostreecontainer --transport=oci --no-signature-verification" >>/usr/share/anaconda/interactive-defaults.ks
+echo "bootc switch --mutate-in-place --enforce-container-sigpolicy --transport registry ghcr.io/ublue-os/bluefin:lts" >>/usr/share/anaconda/post-scripts/switch-container.ks
+
+ln -sf /var/lib/flatpak/repo /flatpak/repo
+
 systemctl disable brew-setup.service
 systemctl disable uupd.timer
 systemctl --global disable podman-auto-update.timer
