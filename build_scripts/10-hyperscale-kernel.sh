e58f4cde11bc56b0f4b4120d18fd144abf5b9d62
Tulip Blossom <tulilirockz@proton.me>
2025-04-09 10:46:12 -0300

    fix: enable hs kernel on image-base script + fix GDX build

--- build_scripts/10-hyperscale-kernel.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -xeuo pipefail
-
-# The hyperscale SIG's kernel straight from their official builds
-
-dnf -y install centos-release-hyperscale-kernel
-dnf config-manager --set-disabled "centos-hyperscale,centos-hyperscale-kernel"
-dnf --enablerepo="centos-hyperscale" --enablerepo="centos-hyperscale-kernel" -y update kernel
