8afa81acc08f868c886d04972f46745a54d6d474
Tulip Blossom <tulilirockz@proton.me>
2025-05-04 16:29:55 -0300

    refactor: use `dnf copr` where feasable
    We couldnt use that command at the start because it segfaulted, it should work fine now! :) Easier to understand, too

--- build_scripts/20-packages.sh
+++ build_scripts/20-packages.sh
@@ -22,7 +22,7 @@ dnf -y install \
 	gum \
 	jetbrains-mono-fonts-all \
 	buildah \
-        xhost
+	xhost
 
 # Everything that depends on external repositories should be after this.
 # Make sure to set them as disabled and enable them only when you are going to use their packages.
@@ -34,9 +34,8 @@ dnf config-manager --set-disabled "tailscale-stable"
 dnf -y --enablerepo "tailscale-stable" install \
 	tailscale
 
-
-dnf config-manager --add-repo "https://copr.fedorainfracloud.org/coprs/ublue-os/packages/repo/epel-$MAJOR_VERSION_NUMBER/ublue-os-packages-epel-$MAJOR_VERSION_NUMBER.repo"
-dnf config-manager --set-disabled "copr:copr.fedorainfracloud.org:ublue-os:packages"
+dnf -y copr enable ublue-os/packages
+dnf -y copr disable ublue-os/packages
 dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages swap \
 	centos-logos bluefin-logos
 
@@ -57,13 +56,13 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages install \
 cp -avf /usr/etc/. /etc
 rm -rvf /usr/etc
 
-dnf config-manager --add-repo "https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/epel-${MAJOR_VERSION_NUMBER}/ublue-os-staging-epel-$MAJOR_VERSION_NUMBER.repo"
-dnf config-manager --set-disabled "copr:copr.fedorainfracloud.org:ublue-os:staging"
+dnf -y copr enable ublue-os/staging
+dnf -y copr disable ublue-os/staging
 # FIXME: gsconnect EPEL10 request: https://bugzilla.redhat.com/show_bug.cgi?id=2349097
 dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:staging install \
 	gnome-shell-extension-{search-light,logo-menu,gsconnect}
 
-dnf config-manager --add-repo "https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/centos-stream-${MAJOR_VERSION_NUMBER}/che-nerd-fonts-centos-stream-${MAJOR_VERSION_NUMBER}.repo"
-dnf config-manager --set-disabled copr:copr.fedorainfracloud.org:che:nerd-fonts
+dnf -y copr enable che/nerd-fonts
+dnf -y copr disable che/nerd-fonts
 dnf -y --enablerepo "copr:copr.fedorainfracloud.org:che:nerd-fonts" install \
 	nerd-fonts
