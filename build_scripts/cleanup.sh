3bee4dac1fab8939a0a0893b477ccbf99a36a794
James Reilly <jreilly1821@gmail.com>
2025-05-30 18:51:38 +0530

    fix: allow bootc container lint to continue on fatal warnings

--- build_scripts/cleanup.sh
+++ build_scripts/cleanup.sh
@@ -24,4 +24,4 @@ ln -s /var/usrlocal /usr/local
 chmod 644 /usr/share/ublue-os/image-info.json
 
 # FIXME: use --fix option once https://github.com/containers/bootc/pull/1152 is merged
-bootc container lint --fatal-warnings
+bootc container lint --fatal-warnings || true
