504eed16c16596945d9bae8bd9505a8174aadcb5
renner <renner0@posteo.de>
2025-06-03 16:19:55 +0200

    feat: add ublue-branding-logos
    See: https://github.com/ublue-os/packages/pull/478
    
    The package is now available in copr
    https://copr.fedorainfracloud.org/coprs/ublue-os/packages/build/9111385/

--- build_scripts/20-packages.sh
+++ build_scripts/20-packages.sh
@@ -48,7 +48,7 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:ublue-os:packages install \
 	ublue-os-signing \
 	ublue-os-udev-rules \
 	ublue-os-update-services \
-	ublue-{motd,fastfetch,bling,rebase-helper,setup-services,polkit-rules,brew} \
+	ublue-{motd,fastfetch,bling,rebase-helper,setup-services,polkit-rules,brew,branding-logos} \
 	uupd \
 	bluefin-*
 
