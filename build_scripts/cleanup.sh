535fb4c48ca77934fed2c167ee8d8d476ed6e2ed
Tulip Blossom <tulilirockz@outlook.com>
2026-01-04 21:01:40 -0300

    chore: remove a few unnecessary steps on build (#991)
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>
    
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_scripts/cleanup.sh
+++ build_scripts/cleanup.sh
@@ -11,14 +11,6 @@ dnf config-manager --set-disabled baseos-compose,appstream-compose
 
 dnf clean all
 
-# Fix SELinux contexts for tuned https://github.com/ublue-os/bluefin-lts/issues/841
-restorecon -rv \
-/etc/tuned \
-/usr/lib/tuned \
-/var/log/tuned \
-/var/lib/tuned
-
-
 rm -rf /.gitkeep
 find /var -mindepth 1 -delete
 find /boot -mindepth 1 -delete
@@ -26,10 +18,6 @@ mkdir -p /var /boot
 
 # Make /usr/local writeable
 ln -s /var/usrlocal /usr/local
-
-# We need this else anything accessing image-info fails
-# FIXME: Figure out why this doesnt have the right permissions by default
-chmod 644 /usr/share/ublue-os/image-info.json
 
 # FIXME: use --fix option once https://github.com/containers/bootc/pull/1152 is merged
 bootc container lint --fatal-warnings || true
