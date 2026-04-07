dc8943c6c36bffc3149a58d10b024bb053efbac7
James Reilly <jreilly1821@gmail.com>
2026-04-07 22:06:40 +0530

    Update 10-packages-image-base.sh

--- build_scripts/overrides/base/10-packages-image-base.sh
+++ build_scripts/overrides/base/10-packages-image-base.sh
@@ -126,9 +126,9 @@ dnf -y install \
 	libcamera{,-{v4l2,gstreamer,tools}}
 
 if [[ "${GNOME_VERSION:-49}" == "50" ]]; then
-    dnf -y install gnome50-el10-compat lgda
+    dnf -y install gnome50-el10-compat libgda
 else
-    dnf -y install gnome49-el10-compat lgda
+    dnf -y install gnome49-el10-compat libgda
 fi
 
 # This package adds "[systemd] Failed Units: *" to the bashrc startup
